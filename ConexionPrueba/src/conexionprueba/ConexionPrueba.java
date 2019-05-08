925238aa03860ea2a4ae6f93b04b82d3736e20dd
DavidVazquezCorrales2 <43586430+DavidVazquezCorrales2@users.noreply.github.com>
2019-05-08 20:57:19 +0200

    Update ConexionPrueba.java

--- ConexionPrueba/src/conexionprueba/ConexionPrueba.java
+++ ConexionPrueba/src/conexionprueba/ConexionPrueba.java
@@ -17,6 +17,8 @@ public class ConexionPrueba {
     public static void main(String[] args) {
         // TODO code application logic here
     /*Creando una Rama en GITHUB*/
+        
+        
     }
     
 }
